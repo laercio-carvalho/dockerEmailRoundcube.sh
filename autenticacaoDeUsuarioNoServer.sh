06088433ddf8322d76c89f7855efb3797e3a808d
Laercio Carvalho Silva <laercio_ti-linux@yahoo.com.br>
2023-05-01 21:30:18 -0300

    Adicionado um arquivo autenticacaoDeUsuarioNoServer.sh para fazer login em um servidor remoto via SSH. Também estou aprendendo e mudando aos poucos a possibilidade de colocar frases no switch case no shellScript

---
+++ autenticacaoDeUsuarioNoServer.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo "Aqui é a parte de autenticação"
+echo "Nela nos conectaremos via SSH para autenticar"
+echo "(BETA)As Ultimas Opções são para instalação de outros serviços. Baixe o arquivo.sh correspondente"
+OpcaoFraseTeste="Opcao Frase de Teste"
+select Autentica in AutenticacaoSSH "$OpcaoFraseTeste" Sair
+do
+echo "Escolheu $Autentica "
+#continue
+case $Autentica in
+AutenticacaoSSH)
+	echo "Digite abaixo o nome de Usuario: "
+  	read -r usuarioServer
+	echo "Coloque o nome do Servidor ou o IP: (Sugestao: 192.168.1.69)"
+	read -r DNSouIP
+	ssh $usuarioServer@$DNSouIP
+	./autenticacaoDeUsuarioNoServer.sh
+	break;;
+"$OpcaoFraseTeste")
+	echo "teste de frase funcionou!"
+	./autenticacaoDeUsuarioNoServer.sh
+	break;;
+Sair)
+  	echo "Saindo..."
+  	break;;
+*)
+  	echo "Opcao invalida!"
+  	echo "Tente novamente"
+  	echo " "
+  	./autenticacaoDeUsuarioNoServer.sh
+  	break;;
+
+esac
+done
